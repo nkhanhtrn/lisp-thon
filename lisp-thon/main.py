bce88e62b1c066643b6324b89a39bd6256871984
Khanh Tran <nkhanh.trn@gmail.com>
2020-07-29 00:56:56 +0700

    build abstract syntax tree

--- lisp-thon/main.py
+++ lisp-thon/main.py
@@ -11,6 +11,40 @@ def tokenize(chars: str) -> list:
     return chars.replace("(", " ( ").replace(")", " ) ").split()
 
 
+def atom(token: str) -> Atom:
+    "Numbers become numbers, everything else is a symbol"
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return Symbol(token)
+
+
+def read_from_tokens(tokens: list) -> Exp:
+    "Read expressions from a sequence of tokens"
+    if not tokens:
+        raise SyntaxError("unexpected EOF")
+
+    token = tokens.pop(0)
+    if token == "(":
+        L = []
+        while tokens[0] != ")":
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)
+        return L
+    elif token == ")":
+        raise SyntaxError("unexpected )")
+    else:
+        return atom(token)
+
+
+def parse(program: str) -> Exp:
+    "Return a Scheme expression from a string"
+    return read_from_tokens(tokenize(program))
+
+
 if __name__ == "__main__":
     program = "(begin (define r 10) (* pi (* r r)))"
-    print(tokenize(program))
+    print(parse(program))
